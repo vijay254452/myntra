068d419bfa9e26d6473578eac6c103c620d1daf6
root <root@ip-172-31-34-123.ap-south-1.compute.internal>
2025-09-03 19:35:03 +0000

    polllo

--- src/main/java/com/myntra/MyntraApplication.java
+++ src/main/java/com/myntra/MyntraApplication.java
@@ -2,12 +2,19 @@ package com.myntra;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyntraApplication {
+public class MyntraApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MyntraApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MyntraApplication.class);
     }
 }
 
