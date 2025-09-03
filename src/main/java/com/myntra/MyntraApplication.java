1702e1ffcc50461e30ee82176cd536371adf054f
root <root@ip-172-31-34-123.ap-south-1.compute.internal>
2025-09-03 19:32:28 +0000

    polllo

---
+++ src/main/java/com/myntra/MyntraApplication.java
@@ -0,0 +1,13 @@
+package com.myntra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyntraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyntraApplication.class, args);
+    }
+}
+
